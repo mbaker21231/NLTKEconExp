4b7fb90ee2939ff306300455108cac51a3652b42
Matthew J Baker <matthew.baker@hunter.cuny.edu>
2015-12-30 10:22:11 -0500

    Notebook editing
    Edited notebooks so that a stand-alone file converter is created, and
    also so that inline printouts aren't so long, as Jonathan suggested.
    Also added a few details here and there to make things more useful.

---
+++ pdf2textConverter.py
@@ -0,0 +1,22 @@
+def pdf2textConverter(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')                                                       #Had to change this line as it had deprecated "file" command
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
